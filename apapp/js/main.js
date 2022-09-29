860f6894dc84f4a767ee7517afd5562289c3fdf2
jessiey2 <90731140+jessiey2@users.noreply.github.com>
2022-09-29 16:50:21 -0400

    initial

---
+++ apapp/js/main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import { albums } from "./albums";
+console.log(albums);
+
+const DOMSelectors = {
+likebutton: document.querySelector(),
+dislikebutton: document.querySelector(),
+randomize: document.querySelector(),
+remove: document.querySelector(),
+likedsongs: document.querySelector(),
+dislikedsongs: document.querySelector(),
+};
